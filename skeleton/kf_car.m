8000af27a22cc5f6c1aca1556f16cf93c09029fc
alexandros filotheou <alefil@kth.se>
2015-11-12 00:16:53 +0100

    Pushes through the report

--- skeleton/kf_car.m
+++ skeleton/kf_car.m
@@ -1,37 +1,37 @@
 % EXAMPLE FROM EL2320
-% 
+%
 % This is an example program to simulate the car example described by the
 % equations
-% 
+%
 % p(k+1) = p(k) + dt * s(k)
 % s(k+1) = s(k) + dt * u(k)
 %
 % where p(k) and s(k) is the position and speed respectively
 %
 % This can be re-written in state-space form as
-% 
+%
 % x(k+1) = A * x(k) + B * u(k)
 %
 % where A = [1 dt; 0 1], B = [0; dt], x=[p; s]
 %
 % We also assume that we measure the position, which gives
 % y(k) = [1 0] * x(k) = C * x(k)
-% 
+%
 % We also add noise, i.e.
-% 
+%
 % x(k+1) = A * x(k) + B * u(k) + G * w(k)
 % y(k) = C * x(k) + D * v(k)
 % where w(k) is process noise and v(k) is measurement noise.
-% 
+%
 % We assume that G is eye(2) (identity matrix) and that D is 1;
-% 
+%
 % We assume that w(k) and v(k) are white, zero-mean and Gaussian
-% 
+%
 % For estimation we use the Kalman Filter
 %
 % xhat(k+1|k) = A * xhat(k|k) + B * u(k)
 % P(k+1|k) = A * P * A^T + G * R * G^T
-% 
+%
 % K(k+1) = P(k+1|k) * C^T * inv(C * P(k+1|k) * C^T + D * Q * D^T)
 % xhat(k+1|k+1) = xhat(k+1|k) + K(k+1) * (y(k+1) - C * xhat(k+1|k))
 % P(k+1|k+1) = P(k+1|k) - K(k+1) * C * P(k+1|k)
@@ -49,7 +49,7 @@ C = [1 0];
 x = [1 0.5]';
 u = 0;
 
-% the simulated noise 
+% the simulated noise
 
 wStdP = 0.01; % Noise on simulated position
 wStdV = 0.1;   % Noise on simulated velocity
@@ -59,7 +59,7 @@ vStd = 0.1;    % Simulated measurement noise on position
 % The Kalman Filter modeled uncertainties and initial values
 
 xhat = [-2 0]';
-P = eye(2)*1;
+P = eye(2);
 G = eye(2);
 D = 1;
 R = diag([0.01^2 0.1^2]);
@@ -95,18 +95,18 @@ for k = 1:n
     KK(:,k) = K;
     PP(:,k+1) = reshape(P,4,1);
 
-    clf, subplot(2,1,1), 
+    clf, subplot(2,1,1),
     plot(X(1,1:(k+1)),'r')
-    hold on, 
+    hold on,
     plot(Xhat(1,1:(k+1)),'b')
     plot(X(1,1:(k+1))-Xhat(1,1:(k+1)),'g')
 %     axis([0 n+5 -2 7])
     title('Position (red: true, blue: est, green: error)')
     %legend('true','est','error')
 
-    subplot(2,1,2), 
+    subplot(2,1,2),
     plot(X(2,1:(k+1)),'r')
-    hold on, 
+    hold on,
     plot(Xhat(2,1:(k+1)),'b')
     plot(X(2,1:(k+1))-Xhat(2,1:(k+1)),'g')
 %     axis([0 n+5 -5 5])
